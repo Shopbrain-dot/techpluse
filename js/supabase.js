7c75c3d8a0ef0eadebf9c3a2c3f94c1805db1fa8
Shopbrain-dot <businessmuhammed41@gmail.com>
2026-08-27 04:09:28 +0100

    Create js/supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://uadixswmymsturrephhq.supabase.co";
+
+const SUPABASE_KEY =
+    "sb_publishable_-SBT1Jtwwth4U8ANzYVlHQ_uLEkunGi";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
