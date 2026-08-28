990b4152ff956f171db066125c73b5b7bba4e328
Shopbrain-dot <businessmuhammed41@gmail.com>
2026-08-28 22:44:57 +0100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,20 +2,7 @@ const SUPABASE_URL = "https://uadixswmymsturrephhq.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_2UO1rGLY4h4XQ7OazgElcw_opq_ZWUX";
 
-window.supabaseClient = null;
-
-function initializeSupabase() {
-    if (!window.supabase) {
-        console.error("Supabase library is not loaded.");
-        return;
-    }
-
-    window.supabaseClient = window.supabase.createClient(
-        SUPABASE_URL,
-        SUPABASE_KEY
-    );
-
-    console.log("Supabase client created successfully.");
-}
-
-initializeSupabase();
+window.supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
